cdee9b74e1647015f3737132b58d4b31b6fb3934
hanfengcan <xiemmin@outlook.com>
2018-04-22 19:02:44 +0800

    过渡效果加入豪华套餐

--- src/renderer/components/dashboard/dashboard.vue
+++ src/renderer/components/dashboard/dashboard.vue
@@ -1,9 +1,15 @@
 <template>
   <div class="dashboard">
-    <template v-for="(v, index) in visual">
-      <visualization :key="index" :name="v.name" :outline="v.outline" :isedit="v.isedit"
-      :type="v.type" :data="v.data"></visualization>
-    </template>
+    <!-- <transition-group
+      v-on:before-enter="tbeforeEnter" v-on:enter="enter" v-on:after-enter="tafterEnter"
+      v-on:before-leave="tbeforeLeave" v-on:leave="leave" v-on:after-leave="tafterLeave"
+    > -->
+    <transition-group name="visualization">
+      <template v-for="(v, index) in visual">
+        <visualization :key="index" :name="v.name" :outline="v.outline" :isedit="v.isedit"
+        :type="v.type" :data="v.data"></visualization>
+      </template>
+    </transition-group>
   </div>
 </template>
 
@@ -25,6 +31,28 @@ export default {
     return {
 
     }
+  },
+  methods: {
+    // tbeforeEnter (el) {
+    //   el.style.opacity = 0
+    // },
+    // enter (el, done) {
+    //   el.style.transition = 'opacity 1s ease-out'
+    //   done()
+    // },
+    // tafterEnter (el) {
+    //   // el.style.opacity = 1
+    // },
+    // tbeforeLeave (el) {
+
+    // },
+    // leave (el, done) {
+    //   el.style.transition = 'opacity 1s ease-in'
+    //   done()
+    // },
+    // tafterLeave (el) {
+    //   el.style.opacity = 0
+    // }
   }
 }
 </script>
@@ -35,5 +63,19 @@ export default {
     /* background: rebeccapurple; */
     height: 100%;
     /* padding: 0 5px 5px 0; */
+  }
+
+  /* 过渡 */
+  .visualization-enter-active {
+    transition: opacity 0.5s ease-out
+  }
+  .visualization-leave-active {
+    transition: opacity 0.5s ease-in;
+  }
+  .visualization-enter, .visualization-leave-to {
+    opacity: 0;
+  }
+  .visualization-enter-to, .visualization-leave {
+    opacity: 1;
   }
 </style>
