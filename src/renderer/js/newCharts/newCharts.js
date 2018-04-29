dc73f8e69f75607c6dffb407dd514634627037b4
hanfengcan <xiemmin@outlook.com>
2018-04-29 14:37:05 +0800

    add a add chart dialog

---
+++ src/renderer/js/newCharts/newCharts.js
@@ -0,0 +1,64 @@
+const pie = require('../../assets/newChartsImg/pie.png')
+const line = require('../../assets/newChartsImg/line.png')
+const bar = require('../../assets/newChartsImg/bar.png')
+const ybar = require('../../assets/newChartsImg/ybar.png')
+
+function random () {
+  let d = new Date()
+  return d.getTime() + '.' + Math.floor(Math.random() * 1000)
+}
+
+function newCharts () {
+  return [
+    {
+      label: '饼图',
+      url: pie,
+      cfg: {
+        index: random(),
+        name: '🍕',
+        type: 'pie',
+        isedit: true,
+        outline: [0, 0],
+        data: [['🍕', '🍔', '🍟', '🌭'], [35, 54, 13, 60]]
+      }
+    },
+    {
+      label: '折线图',
+      url: line,
+      cfg: {
+        index: random(),
+        name: '📈',
+        type: 'line',
+        isedit: true,
+        outline: [0, 0],
+        data: [['食物', '🍕', '🍔', '🍟', '🌭'], ['数量', 35, 54, 13, 60]]
+      }
+    },
+    {
+      label: '柱状图',
+      url: bar,
+      cfg: {
+        index: random(),
+        name: '📊',
+        type: 'bar',
+        isedit: true,
+        outline: [0, 0],
+        data: [['食物', '🍕', '🍔', '🍟', '🌭'], ['数量', 35, 54, 13, 60]]
+      }
+    },
+    {
+      label: '柱状图',
+      url: ybar,
+      cfg: {
+        index: random(),
+        name: '📊',
+        type: 'ybar',
+        isedit: true,
+        outline: [0, 0],
+        data: [['食物', '🍕', '🍔', '🍟', '🌭'], ['数量', 35, 54, 13, 60]]
+      }
+    }
+  ]
+}
+
+export default { newCharts }
---
+++ src/renderer/page/newCharts.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="newcharts">
+    <template v-for="(c, index) in charts">
+      <el-card :key="index" :body-style="{ padding: '10px' }" class="card" shadow="hover">
+        <img :src="c.url" :alt="c.label" class="img">
+        <div style="padding: 5px; overflow: auto;">
+          <span style="float: left; line-height: 40px;">{{c.label}}</span>
+          <el-button type="text" class="button" @click="add(c.cfg)">添加</el-button>
+        </div>
+      </el-card>
+      <!-- <div :key="index">{{c.url}} {{c.label}}</div> -->
+    </template>
+  </div>
+</template>
+
+<script>
+import newcharts from '../js/newCharts/newCharts'
+export default {
+  data () {
+    return {
+      charts: newcharts.newCharts()
+    }
+  },
+  methods: {
+    add (arg) {
+      this.$store.commit('addChart', [arg])
+      this.$emit('addChart')
+    }
+  },
+  mounted () {
+    this.$nextTick(() => {
+    })
+  }
+}
+</script>
+
+<style scoped>
+  .newcharts {
+    position: relative;
+    overflow: auto;
+    /* height: 100%;
+    width: 100%; */
+  }
+
+  .card {
+    float: left;
+    margin: 5px;
+  }
+
+  .img {
+    /* width: 100%; */
+    /* height: 140px; */
+    height: 200px;
+    display: block;
+  }
+
+  .button {
+    float: right;
+  }
+</style>
